c563301f48b5de0ea1ea1a2918b27b4686217548
Liliya <olm@europe.com>
2015-10-19 16:00:00 +0300

    with TemplateView and DetailView

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,44 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic import TemplateView, DetailView
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostListView(TemplateView):
+    template_name = "blog/post_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+        return context
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+#         'published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+#     # return render(request, 'blog/post_list.html')post_list
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostDetailView(DetailView):
+
+    model = Post
+
+    # def get_context_data(self, **pk):
+    #     print(pk)
+    #     context = super(PostDetailView, self).get_context_data(pk)
+    #     context["post"] = get_object_or_404(Post, pk=pk)
+    #     return context
+
+# class PostDetailView(TemplateView):
+#     template_name = "blog/post_detail.html"
+#
+#     def get_context_data(self, pk):
+#         context = super(PostDetailView, self).get_context_data(pk=pk)
+#         context["post"] = get_object_or_404(Post, pk=pk)
+#         return context
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
